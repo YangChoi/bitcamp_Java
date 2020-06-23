d640190fc463c01dbf84ef5b7937748924470c7a
YangChoi <48708746+YangChoi@users.noreply.github.com>
2020-06-23 17:32:21 +0900

    day 48
    mybatis sample end

--- myBatisTest/src/user/service/UserSearchService.java
+++ myBatisTest/src/user/service/UserSearchService.java
@@ -15,30 +15,46 @@ public class UserSearchService implements UserService {
 
 		Scanner scan = new Scanner(System.in);
 
-		UserDAO userDAO = UserDAO.getInstance();
+	
 
-		System.out.print("찾는 방식 : ");
+		System.out.println("찾는 방식 : ");
 		System.out.println("1. 아이디 ");
 		System.out.println("2. 이름 ");
+		System.out.print(">> ");
 
 		String choice = scan.next();
-
+		
+		System.out.println();
+		String searchValue = null;
+		String searchType = null;
 		// 아이디
 		if (choice.equals("1")) {
-			String id = scan.next();
-
-			Map<String, String> map = new HashMap<String, String>();
-			map.put("id", id);
-			userDAO.search(map);
+			System.out.print("아이디를 입력해주세요 : ");
+			searchValue = scan.next();
+			searchType = "id";
+			
 			// 이름
 		} else if (choice.equals("2")) {
-			String name = scan.next();
+			System.out.print("이름을 입력해주세요 : ");
+			searchValue = scan.next();
+			searchType = "name";
 
-			Map<String, String> map = new HashMap<String, String>();
-			map.put("name", name);
-			userDAO.search(map);
+			
 		}
 		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("searchType", searchType);
+		map.put("searchValue", searchValue);
+		
+		UserDAO userDAO = UserDAO.getInstance();
+		List<UserDTO> list = userDAO.search(map);
+		
+		for(UserDTO userDTO : list) {
+			System.out.println(userDTO.getName()+"\t"
+							  +userDTO.getId()+"\t"
+							  +userDTO.getPwd());
+			System.out.println();
+		}
 
 	}
 
